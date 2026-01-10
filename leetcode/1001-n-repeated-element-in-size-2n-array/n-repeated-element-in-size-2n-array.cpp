fe6047224f1d738961d0b905ea073076c143956c
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-01-10 20:56:22 +0530

    Time: 3 ms (38.30%) | Memory: 28.3 MB (84.51%) - LeetSync

---
+++ leetcode/1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n=nums.size()/2-1;
+        sort(nums.begin(),nums.end());
+        int cnt=0;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                cnt++;
+            }
+            if(cnt==n){
+                return nums[i];
+            }
+        }
+        return 0;
+        
+    }
+};
