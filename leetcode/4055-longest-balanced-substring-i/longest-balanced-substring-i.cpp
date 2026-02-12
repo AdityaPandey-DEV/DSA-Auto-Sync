c29742f6579e0ed7159410e08104b76e6876e88c
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-02-12 21:25:37 +0530

    Time: 2880 ms (5.08%) | Memory: 496.1 MB (5.08%) - LeetSync

---
+++ leetcode/4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool Check(vector<int>freq){
+        int common=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0) continue;
+            if(common==0) 
+                common=freq[i];
+            else if(freq[i]!=common){
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    int longestBalanced(string s) {
+        
+        int maxL=0;
+        for(int i=0;i<s.length();i++){
+            vector<int>freq(26,0);
+            for(int j=i;j<s.length();j++){
+                freq[s[j]-'a']++;
+                if(Check(freq)){
+                    maxL=max(maxL,j-i+1);
+                }
+            }
+        }
+        return maxL;
+        
+    }
+};
