efd1aaf3b1335274fb8c43677060896a97cd6ac6
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-02-14 15:23:09 +0530

    Time: 7 ms (44.31%) | Memory: 17.8 MB (43.34%) - LeetSync

---
+++ leetcode/815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+double rec(int i,int j,vector<vector<double>> &dp){
+    // if(i==0&&j==0){
+    //     return dp[0][0];
+    // }
+    if(j<0||j>i){
+        return 0.0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    double leftSide=max(0.0,(rec(i-1,j-1,dp)-1.0)/2.0);
+    double rightSide=max(0.0,(rec(i-1,j,dp)-1.0)/2.0);
+
+    return dp[i][j]=leftSide+rightSide;
+
+
+}
+
+
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>>dp(query_row+1,vector<double>(query_row+1,-1));
+        dp[0][0]=poured;
+        return min(1.0, rec(query_row, query_glass, dp));
+
+
+
+        
+    }
+};
