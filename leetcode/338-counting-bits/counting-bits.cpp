be848d454bb2ba84b55497503391a15aee976d5f
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-02-15 15:52:59 +0530

    Time: 1 ms (37.70%) | Memory: 11.3 MB (29.66%) - LeetSync

---
+++ leetcode/338-counting-bits/counting-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int count(int n) {
+        int count = 0;   // initialize
+        
+        while(n){
+            n = n & (n - 1);  // remove rightmost set bit
+            count++;
+        }
+        
+        return count;   // return result
+    }
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(count(i));
+            
+        }
+        return ans;
+        
+    }
+};
