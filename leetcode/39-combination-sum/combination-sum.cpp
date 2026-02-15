2d80658069f2bc8dc2568542d3b0f44fd7cbdcdf
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-02-15 12:03:00 +0530

    Time: 102 ms (5.07%) | Memory: 72.3 MB (5.71%) - LeetSync

---
+++ leetcode/39-combination-sum/combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    void rec(vector<int>& candidates ,vector<vector<int>>&ans,int i,vector<int>a, int target){
+        if(target==0){
+            ans.push_back(a);
+            return;
+        }
+        if(i==candidates.size()||target<0){
+            return;
+        }
+        
+        rec(candidates,ans,i+1,a,target);
+        a.push_back(candidates[i]);
+        
+        rec(candidates,ans,i,a,target-candidates[i]);
+
+
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>>ans;
+        vector<int>a;
+        int i=candidates.size();
+        rec(candidates,ans,0,a,target);
+        
+        return ans;
+        
+        
+    }
+};
