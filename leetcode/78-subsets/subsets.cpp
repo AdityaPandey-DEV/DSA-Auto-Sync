2af26bbb4dca49c392382a8696d6f4f9ee0fad7e
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-03 10:58:09 +0530

    Time: 1 ms (44.95%) | Memory: 15.7 MB (9.37%) - LeetSync

---
+++ leetcode/78-subsets/subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void rec(vector<int>& nums ,vector<vector<int>>&ans,int i,vector<int>a){
+        if(i<0){
+            ans.push_back(a);
+            return;
+        }
+        
+        rec(nums,ans,i-1,a);
+        a.push_back(nums[i]);
+        
+        rec(nums,ans,i-1,a);
+        // a.pop_back();
+
+
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>a;
+        int i=nums.size();
+        rec(nums,ans,i-1,a);
+        return ans;
+
+
+        
+    }
+};
