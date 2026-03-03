34d54b64d3d7e1ebcd009d5ecb23c58572d5e3ae
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-03 10:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (23.21%) - LeetSync

---
+++ leetcode/73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int,int>>p;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    p.push_back({i,j});
+                    
+                }
+            }
+        }
+        for(int k=0;k<p.size();k++){
+            int i=p[k].first;
+            int j=p[k].second;
+                for(int a=0;a<matrix.size();a++){
+                    matrix[a][j]=0;
+                    
+                }
+                for(int a=0;a<matrix[0].size();a++){
+                    matrix[i][a]=0;
+                    
+                }
+            }
+        
+    }
+};
