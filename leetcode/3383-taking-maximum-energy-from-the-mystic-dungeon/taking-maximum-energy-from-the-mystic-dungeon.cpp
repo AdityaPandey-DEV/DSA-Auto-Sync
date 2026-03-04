00d02237b6e35c5fddad5067fcdd22f97501146a
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-04 13:55:41 +0530

    Time: 139 ms (84.93%) | Memory: 155.1 MB (82.73%) - LeetSync

---
+++ leetcode/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n=energy.size();
+        for(int i=n-k-1;i>=0;i--){
+            energy[i]+=max(INT_MIN,energy[i+k]);
+        }
+        
+        return *max_element(energy.begin(),energy.end());
+        
+        
+    }
+};
