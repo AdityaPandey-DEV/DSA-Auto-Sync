a06df6a425d894542c4080a114e181b7054d13cc
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-04 13:14:59 +0530

    Time: 57 ms (24.36%) | Memory: 16 MB (99.81%) - LeetSync

---
+++ leetcode/1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans=0;
+        for(int n:nums){
+            int sum=0;
+            int sumn=0;
+            for(int i=1;i<=sqrt(n);i++){
+                if(n%i==0){
+                    sumn+=i;
+                    sum++;
+                    if(i!=n/i){
+                        sumn+=n/i;
+                        sum++;
+                    }
+                }  
+            }
+            
+            if(sum==4){
+                ans+=sumn;
+            }
+            sum=0;
+            sumn=0;
+            
+        }
+        
+        return ans;
+    }
+};
