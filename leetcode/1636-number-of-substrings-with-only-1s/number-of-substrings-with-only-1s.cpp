75d7555707da4bcf5a1b9a668f733207153ad021
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-04 13:19:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (14.58%) - LeetSync

---
+++ leetcode/1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,21 @@
+class Solution {
+
+public:
+
+    int numSub(string s) {
+            long long cnt=0;
+            long long ans=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1'){
+                cnt++;
+                ans+=cnt;
+            }
+            else{
+                
+                cnt=0;
+            }
+        }
+        return ans%1000000007;
+        
+    }
+};
