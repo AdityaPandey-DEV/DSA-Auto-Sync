44b921d5fdd117e799ef3b1e48a99d0e66aab17b
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-04 13:31:41 +0530

    Time: 854 ms (11.69%) | Memory: 256.8 MB (11.19%) - LeetSync

---
+++ leetcode/2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    bool isNo(int num) {
+        string s = to_string(num);
+        vector<int> freq(10, 0); 
+        for (char ch : s) freq[ch - '0']++;
+        for (int d = 0; d <= 9; d++) {
+            if (freq[d] > 0 && freq[d] != d)
+                return false;
+        }
+        return true;
+    }
+public:
+    int nextBeautifulNumber(int n) {
+        for (int i = n + 1; i <= 10000000; i++) {
+            if (isNo(i)) return i;
+        }
+        return -1;
+    }
+};
