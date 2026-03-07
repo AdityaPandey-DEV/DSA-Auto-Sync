6387b8bf1a19e0d4660129a9dd2c10a49328f66e
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-07 12:32:15 +0530

    Time: 61 ms (12.82%) | Memory: 53.9 MB (8.59%) - LeetSync

---
+++ leetcode/322-coin-change/coin-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int rec(vector<int>& coins,int amount,int i,vector<vector<int>>&dp){
+        if(i==coins.size()){
+            return INT_MAX;
+        }
+        if(amount==0){
+            return 0;
+        }
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
+        }
+        int take=INT_MAX;
+        if(amount>=coins[i]){
+            take=rec(coins,amount-coins[i],i,dp);
+            if(take!=INT_MAX) take+=1;
+        }
+        int notTake=rec(coins,amount,i+1,dp);
+        return dp[i][amount]=min(take,notTake);
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        int ans=rec(coins,amount,0,dp);
+        if(ans==INT_MAX) return -1;
+        return ans;
+        
+    }
+};
