26eebbca816f1ed8309b67dabd8d7d26aea1d2a8
Aditya Pandey <100739638+AdityaPandey-DEV@users.noreply.github.com>
2026-03-11 17:27:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.22%) - LeetSync

---
+++ leetcode/1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=1;
+        while(m<n){
+            m=m<<1;
+            m=m+1;
+        }
+        int ans=n^m;
+        return ans;
+
+        
+    }
+};
